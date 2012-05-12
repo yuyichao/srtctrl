8d8cd2ee8de95842c11d165e3531a38c870412ce
Yichao Yu <yyc1992@gmail.com>
2012-05-12 15:16:59 -0400

    add locale, errno handling, add send buffer, reset _conn when disconnect async, class method wrapper for capi send()/recv()

--- client/srt_client/srtsock.py
+++ client/srt_client/srtsock.py
@@ -1,9 +1,11 @@
 import os, ctypes
 from socket import * # for flags only
+import locale
+locale.setlocale(locale.LC_ALL, '')
 
 from .srtbase import *
 
-_hdl = ctypes.CDLL(None)
+_hdl = ctypes.CDLL(None, use_errno=True)
 
 def _init():
     _hdl.recv.argtypes = (ctypes.c_int, ctypes.c_void_p,
@@ -16,10 +18,16 @@ def _init():
 def recv(fd, count, flags=0):
     buff = ctypes.create_string_buffer(count)
     rcount = _hdl.recv(fd, buff, count, flags)
+    if rcount < 0:
+        errno = ctypes.get_errno()
+        raise OSError(errno, os.strerror(errno))
     return buff.raw[:rcount]
 
 def send(fd, buff, flags=0):
     rcount = _hdl.send(fd, buff, len(buff), flags)
+    if rcount < 0:
+        errno = ctypes.get_errno()
+        raise OSError(errno, os.strerror(errno))
     return rcount
 
 _init()
@@ -34,6 +42,7 @@ class SrtSock(SrtBase):
         super().__init__(conf=conf)
         self._client = Gio.SocketClient()
         self._conn = None
+        self._send_buff = b''
     @classmethod
     def new_from_fd(klass, fd, conf={}):
         self = klass(conf={})
@@ -59,11 +68,13 @@ class SrtSock(SrtBase):
     def conn_async(self, host, port, cb, *args):
         if self.is_connected():
             return
+        self._send_buff = b''
         self._client.connect_to_host_async(host, port, None,
                                            self._conn_cb, (cb, args))
-    def conn(self, host, port, cb, *args):
+    def conn(self, host, port):
         if self.is_connected():
             return
+        self._send_buff = b''
         try:
             self._conn = self._client.connect_to_host(host, port, None)
         except GLib.GError:
@@ -74,6 +85,7 @@ class SrtSock(SrtBase):
             self._conn = client.connect_to_host_finish(res)
         except GLib.GError:
             self._conn = None
+        self._send_buff = b''
         try:
             cb(self, *args)
         except TypeError:
@@ -83,6 +95,7 @@ class SrtSock(SrtBase):
             return
         self._conn.close(None)
         self._conn = None
+        self._send_buff = b''
     def disconn_async(self, cb, *args):
         if not self.is_connected():
             return
@@ -90,12 +103,19 @@ class SrtSock(SrtBase):
     def _disconn_cb(self, client, res, data):
         cb, args = data
         client.close_finish(res)
+        self._conn = None
+        self._send_buff = b''
         try:
             cb(self, *args)
         except TypeError:
             pass
 
-    def send(self, buff):
-        pass
-    def recv(self, count):
-        pass
+    def _send(self, buff, flags=0):
+        return send(self.get_fd(), buff, flags=flags)
+    def _recv(self, count, flags=0):
+        return recv(self.get_fd(), count, flags=flags)
+    def _try_send_buff(self):
+        try:
+            c = self._send(self._send_buff)
+        except OSError:
+            pass
