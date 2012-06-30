d6b79a7462eb5baa5413f160fe8162568f1ab384
Yichao Yu <yyc1992@gmail.com>
2012-06-30 16:15:00 -0400

    -

--- lib/pywkjs/test/test_helper.py
+++ lib/pywkjs/test/test_helper.py
@@ -4,12 +4,51 @@ from gi.repository import Gtk, WebKit, GLib
 from pywkjs import *
 import os, sys
 
+def printr():
+    _print = print
+    def printr(*arg, **kwarg):
+        end = '\n'
+        if 'end' in kwarg:
+            end = kwarg['end']
+        kwarg['end'] = ''
+        _print('\033[31;1m', end='')
+        _print(*arg, **kwarg)
+        _print('\033[0m', end=end)
+    return printr
+printr = printr()
+
+def _print():
+    _print = print
+    def __print(*arg, **kwarg):
+        end = '\n'
+        if 'end' in kwarg:
+            end = kwarg['end']
+        kwarg['end'] = ''
+        _print('\033[32;1m', end='')
+        _print(*arg, **kwarg)
+        _print('\033[0m', end=end)
+    return __print
+print = _print()
+
+printr('\nTest Start')
+
 def win_obj_clr_cb(helper, frame, obj):
     obj.pyobj = "this is a py str"
     obj.button = button
+    globals()['jseval'] = obj.eval
 
 def load_finished_cb(view, frame):
-    pass
+    print(jseval)
+    print(jseval("[1, 2, 3, 4]"))
+    print(jseval("function a(){return 5;}; a"))
+    func = jseval("function a() {return 5;}; a");
+    print(func())
+    print(jseval("null"))
+    print(jseval("a = {}"))
+
+def console_log(view, msg, line, src):
+    print("%s\n@#%d: %s" % (src, line, msg))
+    return True
 
 Gtk.init([])
 
@@ -30,5 +69,8 @@ helper = Helper(webview)
 helper.connect("window-object-cleared", win_obj_clr_cb)
 webview.load_uri(url)
 webview.connect("load-finished", load_finished_cb)
+webview.connect("console-message", console_log)
 
 Gtk.main()
+
+printr('Test End\n')
