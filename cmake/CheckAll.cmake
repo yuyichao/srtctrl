a33fc75e3e7633919bedf85a2b944654658a99df
Yichao Yu <yyc1992@gmail.com>
2012-07-08 06:54:01 -0400

    license for cmake

--- cmake/CheckAll.cmake
+++ cmake/CheckAll.cmake
@@ -1,21 +1,18 @@
-# Copyright 2012 Yu Yichao
-# yyc1992@gmail.com
+#   Copyright (C) 2012~2012 by Yichao Yu
+#   yyc1992@gmail.com
 #
-# This file is part of alarmd.
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 #
-# alarmd is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 #
-# alarmd is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with alarmd.  If not, see <http://www.gnu.org/licenses/>.
-#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 macro(check_and_add TARGET VAR)
   pkg_check_modules(${VAR} ${ARGN})
