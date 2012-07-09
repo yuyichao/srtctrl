7da11c7c7e2e7ec49b3aa2cba1172ddc36dd8f1e
Yichao Yu <yyc1992@gmail.com>
2012-07-08 21:03:40 -0400

    add wrapper

--- lib/srt_comm/test/test_wrapper2.py
+++ lib/srt_comm/test/test_wrapper2.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+#!/usr/bin/env python
 
 #   Copyright (C) 2012~2012 by Yichao Yu
 #   yyc1992@gmail.com
@@ -16,12 +16,39 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from srt_comm.srtaddr import *
-from srt_comm.srtconn import SrtConn
-from srt_comm.jsonstm import get_json
-from srt_comm.ps import *
-from srt_comm.util import *
-from srt_comm import config
-from srt_comm.module import *
-import locale
-locale.setlocale(locale.LC_ALL, '')
+from srt_comm import *
+
+def new_dict_wrapper():
+    d = {}
+    def getter(key1, key2):
+        return d[key1][key2]
+    def setter(key1, key2, value):
+        if not key1 in d:
+            d[key1] = {}
+        d[key1][key2] = value
+    def p():
+        print(d)
+    return p, new_wrapper2(getter, setter)
+
+p, obj = new_dict_wrapper()
+p2, obj2 = new_dict_wrapper()
+
+p()
+p2()
+obj.a.b = 1
+obj.c.e = 3
+obj.a.j = "asdf"
+obj.c.ll = [1, 2]
+obj.k.a = None
+obj.lll.s = "m"
+obj.lll.s = 1234
+
+obj2.a.b = 188
+obj2.c.d = 3412
+obj2.a.j = "aslllllllll"
+obj2.c.ll = [1, 2, 4]
+obj2.k.a = {}
+obj2.lll.s = "mkkk"
+p()
+p2()
+print(obj.lll.s)
