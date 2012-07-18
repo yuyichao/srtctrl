abfb4931be2869cd232ba7f3085dd09c9e7ba4bb
Yichao Yu <yyc1992@gmail.com>
2012-07-18 12:06:36 -0400

    add timer

---
+++ lib/modules/timer.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+
+#   Copyright (C) 2012~2012 by Yichao Yu
+#   yyc1992@gmail.com
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function, division
+from srt_comm import *
+from gi.repository import GObject, GLib
+
+class SrtTimer(GObject.Object):
+    __gsignals__ = {
+        "alarm": (GObject.SignalFlags.RUN_FIRST,
+                  GObject.TYPE_NONE,
+                  (GObject.TYPE_PYOBJECT,)),
+    }
+    def __init__(self, timeout=1, **kwargs):
+        super(SrtTimer, self).__init__()
+        timeout = float(timeout)
+        GLib.timeout_add_seconds(timeout, self._update_cb)
+        self._active = True
+    def _update_cb(self):
+        if not self._active:
+            return False
+        self.emit("alarm", True)
+        return True
+    def stop(self):
+        self._active = False
+
+setiface.alarm.timer = SrtTimer
