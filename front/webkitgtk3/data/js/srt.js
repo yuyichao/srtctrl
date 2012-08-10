c399e5418b577554d8675539391203a692eaadc4
Yichao Yu <yyc1992@gmail.com>
2012-08-10 08:12:01 -0400

    include jquery-ui

---
+++ front/webkitgtk3/data/js/srt.js
@@ -0,0 +1,11 @@
+function escapeHTML(str) {
+    return $('<div/>').text(str).html()
+};
+$(document).ready(function() {
+    Back.Source.connect("event::move", function (src, evt, div) {
+        div.text(evt);
+    }, $('#mvevent'));
+    $('#quit').click(function (ev) {
+        Back.Source.quit();
+    });
+});
