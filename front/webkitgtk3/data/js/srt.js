9aeeceda14ecb7c48a408e4995601edb59e8da0f
Yichao Yu <yyc1992@gmail.com>
2012-08-10 12:10:09 -0400

    theme button

--- front/webkitgtk3/data/js/srt.js
+++ front/webkitgtk3/data/js/srt.js
@@ -2,11 +2,24 @@ function escapeHTML(str) {
     return $('<div/>').text(str).html();
 };
 $(function () {
-    $("#quit").button();
+    $(".header-button").button();
+    $("#about-dialog").dialog({
+        autoOpen: false,
+        // show: "blind",
+        // hide: "explode",
+        height: 140,
+        modal: true
+    });
+    $("#about-button").click(function () {
+        $("#about-dialog").dialog("open");
+        return false;
+    });
+
     Back.Source.connect("event::move", function (src, evt, div) {
         div.text(evt.toString());
     }, $('#mvevent'));
-    $('#quit').click(function (ev) {
+    $('#quit-button').click(function (ev) {
         Back.Source.quit();
+        return false;
     });
 });
