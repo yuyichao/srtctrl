8049e75a7590bd902dbe832efd0e7a850342e9c4
Yichao Yu <yyc1992@gmail.com>
2012-08-11 19:14:23 -0400

    about dialog size

--- front/webkitgtk3/data/js/about.js
+++ front/webkitgtk3/data/js/about.js
@@ -1,15 +1,23 @@
 $(function () {
     var about_dialog_tabs = $("#about-dialog-tabs");
+    var about_dialog = $("#about-dialog");
+    var about_dialog_content = $("#about-dialog-content");
     var tabs = {
         copy: $("#about-dialog-tab-copy"),
         desc: $("#about-dialog-tab-description")
     };
     about_dialog_tabs.tabs();
-    add_dialog($("#about-button"), $("#about-dialog"), true);
+    add_dialog($("#about-button"), about_dialog, true);
     $(".about-tab-buttons").click(function () {
-        $("#about-dialog-tabs").resize();
+        about_dialog.resize();
     });
-    $("#about-dialog-tabs").resize(function () {
+    about_dialog.resize(function () {
+        about_dialog_content.outerHeight(
+            $(this).height()
+        );
+        return false;
+    });
+    about_dialog_tabs.resize(function () {
         for (var tname in tabs) {
             var height;
             height = (about_dialog_tabs.height()
@@ -17,6 +25,9 @@ $(function () {
                       - tabs[tname].outerHeight()
                       + tabs[tname].height());
             tabs[tname].height(height);
+            console.log(tname);
+            console.log(height);
         }
+        return false;
     });
 });
