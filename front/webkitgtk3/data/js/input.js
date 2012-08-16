3434e430da423d4c3873d8b9dc6fd719d9e08db2
Yichao Yu <yyc1992@gmail.com>
2012-08-16 17:11:54 -0400

    save

--- front/webkitgtk3/data/js/input.js
+++ front/webkitgtk3/data/js/input.js
@@ -1,26 +1,65 @@
 (function () {
     $.extend({
-        select_file: function () {
+        open_file: function (cur_file) {
             var sel_file = null;
             var fc_dlg = UI.Gtk.FileChooserDialog(
                 'Open ... ', null,
                 UI.Gtk.FileChooserAction.OPEN,
-                [UI.Gtk.STOCK_CANCEL, UI.Gtk.ResponseType.CANCEL,
-                 UI.Gtk.STOCK_OPEN, UI.Gtk.ResponseType.OK]);
+                [UI.Gtk.STOCK_OPEN, UI.Gtk.ResponseType.OK,
+                 UI.Gtk.STOCK_CLEAR, UI.Gtk.ResponseType.CLOSE,
+                 UI.Gtk.STOCK_CANCEL, UI.Gtk.ResponseType.CANCEL]);
             fc_dlg.set_default_response(UI.Gtk.ResponseType.OK);
+            if (cur_file)
+                fc_dlg.set_filename(cur_file);
             resp = fc_dlg.run();
-            if (resp == UI.Gtk.ResponseType.OK)
+            if (resp == UI.Gtk.ResponseType.OK) {
                 sel_file =  fc_dlg.get_filename();
+            } else if (resp == UI.Gtk.ResponseType.CANCEL) {
+                sel_file = cur_file;
+            }
             fc_dlg.destroy();
-            uri = UI.GLib.filename_to_uri(sel_file, null);
-            return uri;
+            return sel_file;
+        },
+        save_file: function (cur_file) {
+            var sel_file = null;
+            var fc_dlg = UI.Gtk.FileChooserDialog(
+                'Save ... ', null,
+                UI.Gtk.FileChooserAction.SAVE,
+                [UI.Gtk.STOCK_SAVE, UI.Gtk.ResponseType.OK,
+                 UI.Gtk.STOCK_CLEAR, UI.Gtk.ResponseType.CLOSE,
+                 UI.Gtk.STOCK_CANCEL, UI.Gtk.ResponseType.CANCEL]);
+            fc_dlg.set_default_response(UI.Gtk.ResponseType.OK);
+            if (cur_file)
+                fc_dlg.set_filename(cur_file);
+            resp = fc_dlg.run();
+            if (resp == UI.Gtk.ResponseType.OK) {
+                sel_file =  fc_dlg.get_filename();
+            } else if (resp == UI.Gtk.ResponseType.CANCEL) {
+                sel_file = cur_file;
+            }
+            fc_dlg.destroy();
+            return sel_file;
         }
     });
     var input_types = {
         text: function () {
             return $('<input class="ui-widget-content ui-corner-all"/>');
         },
-        file: function () {
+        open_file: function () {
+            var input = $('<input class="ui-widget-content ui-corner-all"/>');
+            input.click(function () {
+                var $this = $(this);
+                $this.val($.open_file($this.val()));
+            });
+            return input;
+        },
+        save_file: function () {
+            var input = $('<input class="ui-widget-content ui-corner-all"/>');
+            input.click(function () {
+                var $this = $(this);
+                $this.val($.save_file($this.val()));
+            });
+            return input;
         }
     };
     $.extend({
--- front/webkitgtk3/data/js/npoint.js
+++ front/webkitgtk3/data/js/npoint.js
@@ -1,5 +1,7 @@
 $(function () {
     var npoint_dialog_content = $("#npoint-dialog-content");
     npoint_dialog_content.popup("#npoint-button");
-    $("#file-npoint").click($.select_file);
+    $("#file-npoint").click(function () {
+        $(this).val($.save_file($(this).val()));
+    });
 });
